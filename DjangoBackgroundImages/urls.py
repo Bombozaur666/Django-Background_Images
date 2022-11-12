43002bcfb3e9391cbcb725bf1de56e583d13c24c
Bombozaur666 <bombozaur6@gmail.com>
2022-11-12 20:39:40 +0100

    repair api

--- DjangoBackgroundImages/urls.py
+++ DjangoBackgroundImages/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api', include('DjangoBackgroundImages.api.urls')),
+    path('api/', include('DjangoBackgroundImages.api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
